84b0aefaa0d2c065f70c63d3100a3ecf89ac2d0b
Raphael J. Sandor <rsandor@cs.odu.edu>
2019-09-16 15:05:44 -0400

    Will any base as input

--- src/main/java/decimalToBinary/DecimalToBinary.java
+++ src/main/java/decimalToBinary/DecimalToBinary.java
@@ -5,7 +5,7 @@ import java.text.DecimalFormat;
 /**
  * 
  * @author 	Raphael J. Sandor
- * @version 1.0
+ * @version 2.0
  * @since 	2019-09-07
  * 
  * The program decimalToBinary takes an a number from the command line as input.
@@ -17,48 +17,64 @@ public class DecimalToBinary {
 
 	final static int MAX_DIGITS=8; 
 	
+	private int base = 0;
+	//.0001 was the original threshold
+	final double THRESHOLD = .0001;
+	
+	
+	public int getBase() {
+		return base;
+	}
+
+	public void setBase(int base) {
+		this.base = base;
+	}
+
 	public static void main(String[] args) {
-		if (args.length < 1) {
+		if (args.length < 2) {
 			System.out.println("Please run the program again with valid input.");
 			// ends program early.
 			return;
 		}
-		double base10[]= new double[args.length];
-		double base2[] = new double[args.length];
+		
+		int size = args.length -1;
+		double base10[]= new double[size];
+		double otherbase[] = new double[size];
 		
 		
 		/*
 		 * Converts all of the command line arguments to doubles
 		 * c0 is the notation used in the algorithm for user input. 
 		 */
-		for (int i=0; i < args.length; i++) {
-			double c0 = Double.parseDouble(args[i]);
+		for (int i=0; i < args.length-1; i++) {		
+			double c0 = Double.parseDouble(args[i+1]);
 			base10[i] = c0; 
 		}
 		
 		DecimalToBinary solver = new DecimalToBinary();
+		solver.setBase(Integer.parseInt(args[0]));
 		
 		// return the decimal to binary mantissa.
-		base2 = solver.fractionSolver(base10);
-		
-		solver.printOutput(base10, base2);
+		otherbase = solver.fractionSolver(base10);
+
+		solver.printOutput(base10, otherbase);
 	}
 
 	/**
 	 * 
 	 * @param base10 The base 10 input number.
-	 * @param base2  The output of the conversion to base 2. 
+	 * @param otherbase  The output of the conversion to another base. 
+	 * @param base the value of the other base.
 	 */
-	void printOutput(double base10[], double base2[]) {
-		System.out.format("%-13s%-12s", "|  Base 10   |", "  Base 2   |\n");
-		System.out.format("%-13s%-12s", "|:-----------|", ":----------|\n");
+	void printOutput(double base10[], double otherbase[]) {
+		System.out.format("%-13s%-7s%-5s%-7s", "|  Base 10   |", "  Base ", this.getBase(), " |");
+		System.out.format("%-13s%-11s", "\n|:-----------|", ":------------|\n");
 		int x = base10.length;
 		DecimalFormat df = new DecimalFormat("#.########"); 
 		
 		  for (int i = 0 ; i < x; i++) { 
-			 // System.out.format("%-2s%-11s%-2s%-10f%-1s", "| ", base10[i], "| ", base2[i] ,"|\n"); }
 			  	System.out.format("%-2s%-11s%-2s", "| ", base10[i], "| ");
-			  	System.out.format("%-10s%-1s", df.format(base2[i]),"|\n");
+			  	System.out.format("%-10s%-1s", df.format(otherbase[i]),"  |\n");
 
 		  }
 	}
@@ -71,43 +87,91 @@ public class DecimalToBinary {
 	double[] fractionSolver(double dec[]){
 		double base2[] = new double[dec.length];
 		DecimalFormat df = new DecimalFormat("#.########"); 
+		
+		// Need to find out how many place the base needs to be modified 
+		// for bases greater than 10 they require two decimal places 11...99
+		// and of course for bases greater 100 it requires places .###^-1 +###^-2 ... + ####^-n
+		int baseModifier = 0;
+		double baseGreaterThan10 = this.getBase();
+		while (baseGreaterThan10 > 10 ) {
+				baseGreaterThan10 = baseGreaterThan10 / 10; 
+				baseModifier++;
+		}
+				
+		// Given the size and convoluted nature of this code,
+		// if I had more time I would break this up with more helper functions
 		for (int i = 0; i < dec.length; i++)
 		{
 			double ci = dec[i];
-			double bi = (int) ci;
-			
-			base2[i] = bi; 
+			double bi = 0;
 			
 			// starting at 1 because that would (.1)base2
 			for (int binDig= 1; binDig < MAX_DIGITS+1; binDig++)
 			{
 				
-				bi =0;
+				bi = 0;
 				if (ci == 0)
 				{
 					break;
 				}
+				System.out.println("0) ci: " + ci);
+				// Check if this is a perfect 1/ base
+				double mathFormulaForBase = (1.0/(this.getBase()));
+				double result;
+	
+				if (this.getBase() > 10) {
+					result = Math.abs(ci - mathFormulaForBase);
+				}
+				else 
+					result = Math.abs(ci - mathFormulaForBase);
+				double testfor1ofBase = Math.abs((ci/10) - mathFormulaForBase);
+
+				ci = ci*this.getBase();		
+	
 				
-				ci = ci*2;				
+				if (result < THRESHOLD && this.getBase() < 10) {
+					bi = 1;
+					bi = bi * Math.pow(10, -binDig);
+					base2[i] += bi;
+					break;
+				}
+				if (result < THRESHOLD && this.getBase() > 10) {
+					bi = 1;
+					if (binDig == 1)
+						bi = bi * Math.pow(10, -binDig*binDig-1);
+					else
+						bi = bi * Math.pow(10, -binDig*binDig);
+					base2[i] += bi;
+					break;
+				}
+				else if (testfor1ofBase <THRESHOLD && this.getBase() > 10)
+				{
+					bi = 1;
+					bi = bi * Math.pow(10, -binDig*binDig-1);
+					base2[i] += bi;
+					break;
+				}
+
+
 				
 				// Take only the integer value.
 				bi = (int) ci;
+				
 
 				// Leave only the fractional value.
 				ci = ci - bi;
 				
+			
 				// Noticed strange behavior with subtracting the integer. 
 				// e.g ci = .2400000005 to eliminate I said just round to the 7th,
 				// ought be good enough.... 
 				ci = Double.parseDouble(df.format(ci));
 
-				bi = (int)bi;
-				if (bi > 0)
-				{
-					// Now multiply the integer bi by 10^binDig.
-					bi =  Math.pow(10, -binDig);
-				}
 				
+				 if (binDig > 1 && this.getBase() > 10) {  baseModifier++; }
+				 
+				// Now multiply the integer bi by 10^binDig.
+				bi = bi * Math.pow(10, -binDig-baseModifier);
 				
 				// Lastly add that value to that base2.
 				base2[i] += bi;
--- src/test/java/decimalToBinary/DecimalToBinaryTest.java
+++ src/test/java/decimalToBinary/DecimalToBinaryTest.java
@@ -10,28 +10,62 @@ public class DecimalToBinaryTest {
 
 
 	
+	
 	  @Test public void test1() { double base10numbers[] = new double[] { .5 };
-	  double checkedBase2Answers[] = new double[] { .1 }; double base2[] =
-	  solver.fractionSolver(base10numbers); 
-	  assertEquals(base2[0], checkedBase2Answers[0], .0); }
+	  double checkedBase2Answers[] = new double[] { .1 }; solver.setBase(2); double
+	  base2[] = solver.fractionSolver(base10numbers); assertEquals(base2[0],
+	  checkedBase2Answers[0], .0); }
 	 
 	  @Test public void test2() { double base10numbers[] = new double[]{.15};
-	  double checkedBase2Answers[] = new double[]{.00100110}; double base2[] =
+	  double checkedBase2Answers[] = new double[]{.00100110}; solver.setBase(2); double base2[] =
 	  solver.fractionSolver(base10numbers);
 	  assertEquals(checkedBase2Answers[0], base2[0],0); }
 	  
 	  @Test public void test3() { double base10numbers[] = new double[]{.33};
-	  double checkedBase2Answers[] = new double[]{.0101010}; double base2[] =
+	  double checkedBase2Answers[] = new double[]{.0101010}; solver.setBase(2); double base2[] =
 	  solver.fractionSolver(base10numbers);
 	  assertEquals(checkedBase2Answers[0], base2[0], 0 ); }
 	  
 	  @Test public void test4() { double base10numbers[] = new double[]{.10};
-	  double checkedBase2Answers[] = new double[]{.00011001}; double base2[] =
+	  double checkedBase2Answers[] = new double[]{.00011001}; solver.setBase(2); double base2[] =
 	  solver.fractionSolver(base10numbers);
 	  assertEquals(checkedBase2Answers[0], base2[0],0); }
 	  
 	  @Test public void test5() { double base10numbers[] = new double[]{.25};
-	  double checkedBase2Answers[] = new double[]{.01}; double base2[] =
+	  double checkedBase2Answers[] = new double[]{.01}; solver.setBase(2); double base2[] =
 	  solver.fractionSolver(base10numbers);
 	  assertEquals(checkedBase2Answers[0], base2[0],0); }
+	  
+	  
+	  @Test public void base3_test1_1_divided_by_base() { double base10numbers[] = new double[] { .3333 };
+	  double checkedBase3Answers[] = new double[] { .1 }; solver.setBase(3); double
+	  base3[] = solver.fractionSolver(base10numbers); assertEquals(base3[0],
+	  checkedBase3Answers[0], .0); }
+	  
+	  @Test public void base3_test2_1_divided_by_base_squared() { double base10numbers[] = new double[] { .1111 };
+	  double checkedBase3Answers[] = new double[] { .01 }; solver.setBase(3); double
+	  base3[] = solver.fractionSolver(base10numbers); assertEquals(base3[0],
+	  checkedBase3Answers[0], .0); }
+	  
+	  
+	  @Test public void base3_test3() { double base10numbers[] = new double[] { .25 };
+	  double checkedBase3Answers[] = new double[] { .02020202 }; solver.setBase(3); double
+	  base3[] = solver.fractionSolver(base10numbers); assertEquals(base3[0],
+	  checkedBase3Answers[0], .0); }
+	  
+	  @Test public void base3_test4() { double base10numbers[] = new double[] { .50 };
+	  double checkedBase3Answers[] = new double[] { .11111111 }; solver.setBase(3); double
+	  base3[] = solver.fractionSolver(base10numbers); assertEquals(base3[0],
+	  checkedBase3Answers[0], .0); }
+	  
+	  @Test public void base11_test1_1_divided_by_base() { double base10numbers[] = new double[] { .090909 };
+	  double checkedBase11Answers[] = new double[] { .01 }; solver.setBase(11); double
+	  base11[] = solver.fractionSolver(base10numbers); assertEquals(base11[0],
+	  checkedBase11Answers[0], .01); }
+	  
+	  @Test public void base11_test1_1_divided_by_base_sqrd() { double base10numbers[] = new double[] { .0082644 };
+	  double checkedBase11Answers[] = new double[] { .0001 }; solver.setBase(11); double
+	  base11[] = solver.fractionSolver(base10numbers); assertEquals(base11[0],
+	  checkedBase11Answers[0], .01); }
+	  
 }
